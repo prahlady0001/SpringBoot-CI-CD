0fbfce57a7ee8d86bb58bb31163d4e1e39b47dc5
Prahlad <prahlady0001@gmail.com>
2025-12-26 16:34:44 +0530

    Test CD deployment

--- src/main/java/com/spring_boot/controller/Controller.java
+++ src/main/java/com/spring_boot/controller/Controller.java
@@ -9,6 +9,6 @@ public class Controller {
 
     @GetMapping("/")
     public String print(){
-        return "Hello AWS !!! Hello World";
+        return "Hello AWS !!! Hello World CI/CD pipeline working";
     }
 }
