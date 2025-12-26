fe78179705d78049e6e07ef27173dd6f4affdef3
Prahlad <prahlady0001@gmail.com>
2025-12-26 17:28:29 +0530

    Add some message

--- src/main/java/com/spring_boot/controller/Controller.java
+++ src/main/java/com/spring_boot/controller/Controller.java
@@ -9,6 +9,6 @@ public class Controller {
 
     @GetMapping("/")
     public String print(){
-        return "Hello AWS !!! Hello World CI/CD pipeline working";
+        return "Hello AWS !!! Hello World CI/CD pipeline working and Webhook auto Trigger";
     }
 }
