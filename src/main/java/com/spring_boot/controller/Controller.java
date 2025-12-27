f71ea96d1291881a6b23566aaad6d2cd477c1b22
Prahlad <prahlady0001@gmail.com>
2025-12-27 15:08:18 +0530

    Changes on controller change mapping  and add HTML and CSS file

--- src/main/java/com/spring_boot/controller/Controller.java
+++ src/main/java/com/spring_boot/controller/Controller.java
@@ -12,7 +12,7 @@ public class Controller {
         return "VERSION = NEXT-DEPLOY";
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public String home() {
         return "NEW VERSION DEPLOYED ✅";
     }
