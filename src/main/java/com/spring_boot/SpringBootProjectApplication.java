2c51befa14d9f8a3570ab9253d8b91bf944eb672
Prahlad <prahlady0001@gmail.com>
2025-12-27 13:45:43 +0530

    Changes Main classes

--- src/main/java/com/spring_boot/SpringBootProjectApplication.java
+++ src/main/java/com/spring_boot/SpringBootProjectApplication.java
@@ -3,7 +3,7 @@ package com.spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.spring_boot.controller")
+@SpringBootApplication
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
